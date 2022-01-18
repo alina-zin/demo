e05ebd364fea3150d8239e8dff8aa21fd1a18491
alina-zin <73988609+alina-zin@users.noreply.github.com>
2022-01-18 15:35:37 +0200

    currencyconvert

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [euros, setEuros] = useState('');
+  const [pounds, setPounds] = useState(0);
+
+  function change(text) {
+    setEuros(text);
+    const  result = euros.replace(',','.') * 0.9;
+    setPounds(result);
+  }
+
+/*   function calculate() {
+    const  result = euros.replace(',','.') * 0.9;
+    setPounds(result);
+  } */
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Euros</Text>
+      {/* <TextInput style={styles.field} value={euros}
+        onChangeText={text => setEuros(text)} keyboard-type='decimal-pad'></TextInput> */}
+      <TextInput style={styles.field} value={euros}
+        onChangeText={text => change(text)} keyboard-type='decimal-pad'></TextInput>
+        <Text style={styles.field}>Pounds</Text>
+        <Text style={styles.field}>{pounds.toFixed(2)}</Text>
+        {/* <Button onPress={calculate} title="Calculate"></Button> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10
   },
+
+  field: {
+    marginBottom: 10
+  }
 });
